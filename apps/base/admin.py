2d6829f1914751679cf997e5545b59d99e64def0
lizaintp <intpliza@gmail.com>
2024-05-12 13:24:46 +0600

    potfolio-page, portfolio, post, services-page, services updates

--- apps/base/admin.py
+++ apps/base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from apps.base import models
 
 # Register your models here.
+class SettingsFilterAdmin(admin.ModelAdmin):
+    list_filter = ('title', )
+    list_display = ('title', 'insta','facebook','twitter','pinterest','email','phone')
+    search_fields = ('title', 'insta','facebook','twitter','pinterest','email','phone')
+admin.site.register(models.Settings, SettingsFilterAdmin)
--- apps/base/views.py
+++ apps/base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from apps.base import models
 
 # Create your views here.
 def index(request):
+    settings = models.Settings.objects.all()
     return render(request, 'base/index.html', locals())
 
 def about(request):
